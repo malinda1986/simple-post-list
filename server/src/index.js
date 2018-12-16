72cdbbd64f6f9bd1d822e5d3a58f4dd47a2b717f
Malinda <malinda@Ruwans-MacBook-Pro.local>
2018-12-16 10:06:27 +0530

    SpaceX initial commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
 // make sure env is set properly
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-mongoose.set('debug', true);
 
 const profileService = require('./app');
 const Server = require('./app/server');
